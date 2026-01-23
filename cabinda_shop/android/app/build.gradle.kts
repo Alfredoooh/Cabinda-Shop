9c8b76e144491274e795cde0fe89825a2ccc2611
Alfredoooh <alfredopjonas@gmail.com>
2026-01-24 00:12:20 +0100

    Atualizar o build.gradle.kts

--- cabinda_shop/android/app/build.gradle.kts
+++ cabinda_shop/android/app/build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     id("com.android.application")
     id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
 
@@ -20,21 +19,54 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.cabinda_shop"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        minSdk = 24  // Aumenta para permitir mais otimizações
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
+        
+        // SÓ arm64 - reduz 50% do tamanho
+        ndk {
+            abiFilters.clear()
+            abiFilters += "arm64-v8a"
+        }
     }
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
+            
+            // ProGuard - remove código não usado
+            isMinifyEnabled = true
+            isShrinkResources = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+            
+            // Compressão MÁXIMA
+            ndk {
+                debugSymbolLevel = "NONE"
+            }
+            
+            isZipAlignEnabled = true
+            isCrunchPngs = true
+        }
+    }
+    
+    // Remove arquivos desnecessários do APK
+    packagingOptions {
+        resources {
+            excludes += setOf(
+                "META-INF/**",
+                "kotlin/**",
+                "**.properties",
+                "**.bin",
+                "DebugProbesKt.bin"
+            )
+        }
+        jniLibs {
+            useLegacyPackaging = false
         }
     }
 }
